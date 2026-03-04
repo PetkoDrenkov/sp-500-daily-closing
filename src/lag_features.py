e42cfc521b0a537dc1ada6ed4311aa9fd2a5f370
PetkoDrenkov <petko727150@gmail.com>
2026-03-04 16:21:25 +0200

    Update lag_features.py

--- src/lag_features.py
+++ src/lag_features.py
@@ -6,8 +6,8 @@ sp.drop("Date",axis=1,inplace=True)
 sp["returns"] = np.log(sp["Close"]) - np.log(sp["Close"].shift(1))
 
 
-""" max value for lim is 15,and I won't allow for
-greater lag depth for replication purposes (identical size of sets)"""
+""" max value for lim is 15, and I won't allow for
+greater lag depth for replication purposes (identical sizes of sets)"""
 def returns_lagger(df,lim):
     if lim > 15:
         raise ValueError("Limit should NOT be larger than 15")
@@ -15,5 +15,7 @@ def returns_lagger(df,lim):
         for x in range(1,lim+1):
             df[f"lag_{x}"] = df["returns"].shift(x)
         df = df.iloc[16:]
-        return df.drop("Close",axis=1)
+        df.drop("Close",axis=1,inplace=True)
+        return df
+
 
